014e96688e0f38fbd450c31faed339b854291f83
NKM6795 <31067906+NKM6795@users.noreply.github.com>
2019-04-03 12:03:00 +0300

    Added SinglyLinkedTest

---
+++ Chess on a four-linked list/Test/SinglyLinkedTest.cpp
@@ -0,0 +1,94 @@
+#include "pch.h"
+#include "../Chess on a four-linked list/SinglyLinked.h"
+
+
+TEST(WorkWithElements, PushBackElements)
+{
+	//arrange
+	SinglyLinked list;
+
+	for (int i = 0; i < 100; ++i)
+	{
+		//action
+		list.pushBack(i, i);
+
+		//assert
+		EXPECT_EQ(i + 1, list.getSize());
+	}
+}
+
+
+TEST(WorkWithElements, PopFrontElements)
+{
+	//arrange
+	SinglyLinked list;
+
+	//action
+	for (int i = 0; i < 100; ++i)
+	{
+		list.pushBack(i, i);
+	}
+
+	for (int i = 100 - 1; i >= 0; --i)
+	{
+		//action
+		list.popFront();
+
+		//assert
+		EXPECT_EQ(i, list.getSize());
+	}
+
+	//action
+	list.popFront();
+
+	//assert
+	EXPECT_EQ(0, list.getSize());
+}
+
+
+TEST(WorkWithElements, clearList)
+{
+	//arrange
+	SinglyLinked list;
+
+	for (int j = 0; j < 10; ++j)
+	{
+		//action
+		for (int i = 0; i < j; ++i)
+		{
+			list.pushBack(i, i);
+		}
+		
+		//assert
+		EXPECT_EQ(j, list.getSize());
+
+		//action
+		list.clear();
+
+		//assert
+		EXPECT_EQ(0, list.getSize());
+	}
+}
+
+
+TEST(WorkWithElements, correctElement)
+{
+	//arrange
+	SinglyLinked list;
+
+	//action
+	for (int i = 0; i < 100; ++i)
+	{
+		list.pushBack(i, i);
+	}
+
+	//arrange
+	SinglyNode *node = list.getHead();
+
+	//assert
+	for (int i = 0; i < 100; ++i, node = node->next)
+	{
+		EXPECT_EQ(i, node->i);
+		EXPECT_EQ(i, node->j);
+	}
+}
